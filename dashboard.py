88b66fa7beab895541f2b4bc81b152f7f08eb815
syira-commits <mulyasyira555@gmail.com>
2025-10-23 16:18:55 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -111,7 +111,7 @@ if menu in ["Deteksi Objek (YOLO)", "Klasifikasi Gambar"]:
                 st.write(f"🧮 Edge Density: {edge_density:.6f}")
 
                 # Ambang batas empiris (bisa kamu ubah nanti)
-                EDGE_THRESHOLD = 0.004
+                EDGE_THRESHOLD = 0.002
 
                 if edge_density < EDGE_THRESHOLD:
                     st.warning("🩺 Gambar terlalu homogen — kemungkinan gambar sel, bukan Cocopham/Sprite.")
